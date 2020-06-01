e88627975dd1a4f3bb3f80bc7f27fadace183db2
smarto <mtw9411@naver.com>
2020-06-01 12:29:01 +0900

    problem String 5622

---
+++ baekjoon/src/problem/String_5622.java
@@ -0,0 +1,30 @@
+package problem;
+
+import java.util.Scanner;
+
+public class String_5622 {
+
+	public static void main(String[] args) {	// 백준 5622번
+			Scanner sc = new Scanner(System.in);
+		
+			int result = 0;	// 제일 가까운 번호 2초
+			int num;	// 입력 문자를 숫자로 변환
+			
+			String input = sc.nextLine();	// 입력받은 문자열
+			
+			for(int i=0; i<input.length(); i++) {	// 문자열 길이만큼 반복
+				num = input.charAt(i) - 65;
+				if(num >= 18) {
+					num--;
+					if(num == 24) 
+						num--;
+				}
+				result += num/3 + 3;
+			}
+			
+			System.out.println(result);
+	
+			sc.close();
+	}
+
+}
