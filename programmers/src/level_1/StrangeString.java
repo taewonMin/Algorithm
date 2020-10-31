916df6fdd25d4b9521f3121cdb1402ae192f947c
smarto <mtw9411@naver.com>
2020-11-01 00:09:03 +0900

    이상한 문자 만들기

---
+++ programmers/src/level_1/StrangeString.java
@@ -0,0 +1,25 @@
+package level_1;
+
+public class StrangeString {
+	class Solution {
+	    public String solution(String s) {
+	    	String answer = "";
+	        String[] temp = s.split("");
+	        int cnt = 0;
+	        for(int i=0; i<temp.length; i++){
+	    		if(temp[i].equals(" ")){
+	    			answer += temp[i].toLowerCase();
+	    			cnt = 0;
+	    		}else{
+	    			if(cnt%2==0){	// 짝수번째 문자이면
+	        			answer += temp[i].toUpperCase();
+	        		}else{
+	        			answer += temp[i].toLowerCase();
+	            	}
+	    			cnt++;
+	    		}
+	        }
+	        return answer;
+	    }
+	}
+}
