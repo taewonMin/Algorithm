0106d6a9dca960111ffe1e8f092480929d1130c2
smarto <mtw9411@naver.com>
2020-10-31 23:06:46 +0900

    예산

---
+++ programmers/java/level_1/BudgetTest.java
@@ -0,0 +1,19 @@
+package java.level_1;
+
+import java.util.Arrays;
+
+public class BudgetTest {
+	class Solution {
+	    public int solution(int[] d, int budget) {
+	        int answer = 0;
+	        Arrays.sort(d);
+	        for(int i=0; i<d.length; i++){
+	        	budget -= d[i];
+	        	if(budget<0)
+	        		break;
+	    		answer++;
+	        }
+	        return answer;
+	    }
+	}
+}
