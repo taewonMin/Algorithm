6c3165b1a02241fc441129bf592c29fe4164e52d
smarto <mtw9411@naver.com>
2022-05-06 18:19:45 +0900

    입출력과 사칙연산 / ??!

---
+++ baekjoon/src/problem/InputOutput/InputOutput_10926.java
@@ -0,0 +1,22 @@
+package problem.InputOutput;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// ??!
+public class InputOutput_10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        bw.write(input+"??!");
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
