607606639064a1cf79897f7b0691539f303ee5b9
smarto <mtw9411@naver.com>
2022-06-14 23:27:28 +0900

    수학 / A+B 2

---
+++ baekjoon/src/problem/Math1/Math1_2558.java
@@ -0,0 +1,14 @@
+package problem.Math1;
+
+import java.util.Scanner;
+
+// A+B 2
+public class Math1_2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt()+sc.nextInt());
+
+        sc.close();
+    }
+}
