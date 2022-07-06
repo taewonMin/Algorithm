fcad8e6794b816d02c355a1e1339c6e957fc74fc
smarto <mtw9411@naver.com>
2022-07-07 00:32:34 +0900

    수학 / 엄청난 부자2

--- baekjoon/src/problem/Math1/Math1_1271.java
+++ baekjoon/src/problem/Math1/Math1_1271.java
@@ -1,18 +1,18 @@
 package problem.Math1;
 
+import java.math.BigInteger;
 import java.util.Scanner;
-
 
 // 엄청난 부자2
 public class Math1_1271 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
 
-        System.out.println(n/m);
-        System.out.println(n%m);
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
 
         sc.close();
     }
