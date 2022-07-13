6517805da3ef07a5f05990b8cd595eb230c49e87
smarto <mtw9411@naver.com>
2022-07-13 20:27:15 +0900

    구현 / 별 찍기 - 5

---
+++ baekjoon/src/problem/Implement/Imple_2442.java
@@ -0,0 +1,26 @@
+package problem.Implement;
+
+import java.util.Scanner;
+
+// 별 찍기 - 5
+public class Imple_2442 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*i+1; j++){
+                System.out.print("*");
+            }
+            for(int j=0; j<N-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
