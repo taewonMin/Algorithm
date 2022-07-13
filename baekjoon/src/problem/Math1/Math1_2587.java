a0dd5582e2d82ca78a6c8e89059e90e4b7b8ccd8
smarto <mtw9411@naver.com>
2022-07-13 19:35:44 +0900

    수학 / 대표값2

---
+++ baekjoon/src/problem/Math1/Math1_2587.java
@@ -0,0 +1,25 @@
+package problem.Math1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 대표값2
+public class Math1_2587 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+        int sum = 0;
+        for(int i=0; i<5; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(sum / 5);
+        System.out.println(arr[2]);
+
+        sc.close();
+    }
+}
