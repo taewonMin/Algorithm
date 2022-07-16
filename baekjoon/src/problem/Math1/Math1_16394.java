c06fc80898b92164a364e8d198aa7085e360684c
smarto <mtw9411@naver.com>
2022-07-16 10:21:23 +0900

    수학 / 홍익대학교

---
+++ baekjoon/src/problem/Math1/Math1_16394.java
@@ -0,0 +1,15 @@
+package problem.Math1;
+
+import java.util.Scanner;
+
+// 홍익대학교
+public class Math1_16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+        System.out.println(year-1946);
+
+        sc.close();
+    }
+}
