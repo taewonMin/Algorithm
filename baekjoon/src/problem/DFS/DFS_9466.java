24fd74fe48c290ce609ded0ea299f218912bf944
smarto <mtw9411@naver.com>
2022-07-21 12:42:45 +0900

    DFS / 텀 프로젝트

--- baekjoon/src/problem/DFS/DFS_9466.java
+++ baekjoon/src/problem/DFS/DFS_9466.java
@@ -1,4 +1,4 @@
-package problem.BFS;
+package problem.DFS;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 // 텀 프로젝트
-public class BFS_9466 {
+public class DFS_9466 {
     public static int[] student, state;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
