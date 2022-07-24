64c0aa4c68bbaf5a7332384a87601c29884bf8ff
smarto <mtw9411@naver.com>
2022-07-24 21:39:19 +0900

    수학 / 초콜릿 자르기

---
+++ baekjoon/src/problem/Math1/Math1_2163.java
@@ -0,0 +1,16 @@
+package problem.Math1;
+
+import java.util.Scanner;
+
+// 초콜릿 자르기
+public class Math1_2163 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println(N*M-1);
+
+        sc.close();
+    }    
+}
