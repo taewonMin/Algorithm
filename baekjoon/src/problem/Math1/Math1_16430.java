d55c7fe8738b3908b0b41ba93d09ea4d351d55dc
smarto <mtw9411@naver.com>
2022-07-27 23:48:12 +0900

    수학 / 제리와 톰

---
+++ baekjoon/src/problem/Math1/Math1_16430.java
@@ -0,0 +1,16 @@
+package problem.Math1;
+
+import java.util.Scanner;
+
+// 제리와 톰
+public class Math1_16430{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(B-A+" "+B);
+
+        sc.close();
+    }
+}
