f518e5fc37a9477fb59dd8d904753acdc7feee5e
smarto <mtw9411@naver.com>
2022-07-27 20:54:15 +0900

    level2 / 완전탐색 / 카펫

---
+++ programmers/src/level_2/BruteForce/Carpet.java
@@ -0,0 +1,23 @@
+package level_2.BruteForce;
+
+// 카펫
+public class Carpet{
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] res = sol.solution(24, 24);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
+    }
+
+    static class Solution {
+        public int[] solution(int brown, int yellow) {
+            int[] answer = {};
+
+            int b = brown/2 + 2;
+            int d = (int) Math.sqrt(Math.pow(b, 2)-4*(brown+yellow));
+            answer = new int[]{(b+d)/2, (b-d)/2};
+
+            return answer;
+        }
+    }
+}
