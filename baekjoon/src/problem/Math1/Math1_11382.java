420d726325abec9349babc8b1a7df89a9f051d02
smarto <mtw9411@naver.com>
2022-07-28 23:17:15 +0900

    수학 / 꼬마 정민

---
+++ baekjoon/src/problem/Math1/Math1_11382.java
@@ -0,0 +1,17 @@
+package problem.Math1;
+
+import java.util.Scanner;
+
+// 꼬마 정민
+public class Math1_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(A+B+C);
+
+        sc.close();
+    }
+}
