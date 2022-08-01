2b82d2461f86d62c3dc96f52ffe185a301654647
smarto <mtw9411@naver.com>
2022-08-01 22:26:38 +0900

    백트래킹 / 소문난 칠공주 (dfs)

--- baekjoon/src/problem/BackTracking/BackTracking_1941.java
+++ baekjoon/src/problem/BackTracking/BackTracking_1941.java
@@ -3,7 +3,6 @@ package problem.BackTracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,7 +10,8 @@ import java.util.Queue;
 public class BackTracking_1941{
     public static char[][] pos = new char[5][5];
     public static boolean[][] selected = new boolean[5][5];
-    public static boolean[][] visited = new boolean[5][5];
+    public static int[][] visited = new int[5][5];
+    public static int visit = 1;
     public static int result = 0;
     public static int[] dx = {-1,0,1,0};
     public static int[] dy = {0,1,0,-1};
@@ -22,7 +22,7 @@ public class BackTracking_1941{
             pos[i] = br.readLine().toCharArray();
         }
 
-        combination(0, 0);
+        combination(0, 0, 0);
 
         System.out.println(result);
 
@@ -30,19 +30,16 @@ public class BackTracking_1941{
     }
 
     // 25개 자리중 7개 선택
-    public static void combination(int cnt, int num){
+    public static void combination(int cnt, int num, int yCnt){
         if(cnt==7){
-            for(boolean[] visit : visited){
-                Arrays.fill(visit, false);
-            }
-            for(int i=0; i<5; i++){
-                for(int j=0; j<5; j++){
-                    if(selected[i][j] == true){
-                        bfs(i, j);
-                        return;
-                    }
+            if(yCnt < 4){
+                // bfs((num-1)/5, (num-1)%5);
+                if(dfs((num-1)/5, (num-1)%5) == 7){
+                    result++;
                 }
+                visit++;
             }
+            return;
         }
         if(num==25){
             return;
@@ -51,41 +48,46 @@ public class BackTracking_1941{
         int x = num/5;
         int y = num%5;
         selected[x][y] = true;
-        combination(cnt+1, num+1);  // 선택
+        combination(cnt+1, num+1, yCnt+(pos[x][y]=='Y' ? 1:0));  // 선택
         selected[x][y] = false;
-        combination(cnt, num+1); // 미선택
+        combination(cnt, num+1, yCnt); // 미선택
     }
 
     // 선택한 7개 위치가 연결되어있는지 확인
     public static void bfs(int x, int y){
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{x,y});
-        visited[x][y] = true;
+        visited[x][y] = visit;
 
         int cnt = 1;    // 연결된 개수
-        int yCnt = pos[x][y]=='Y' ? 1 : 0;
         while(!queue.isEmpty()){
             int[] node = queue.poll();
             for(int i=0; i<4; i++){
                 int posX = node[0]+dx[i];
                 int posY = node[1]+dy[i];
-                if(boundaryCheck(posX, posY) && !visited[posX][posY] && selected[posX][posY]){
-                    visited[posX][posY] = true;
-                    if(pos[posX][posY]=='Y'){
-                        if(yCnt==3){
-                            return;
-                        }
-                        yCnt++;
-                    }
+                if(boundaryCheck(posX, posY) && visited[posX][posY]!=visit && selected[posX][posY]){
+                    visited[posX][posY] = visit;
                     queue.offer(new int[]{posX, posY});
                     cnt++;
                 }
             }
-            if(cnt==7){
-                result++;
-                break;
+        }
+        if(cnt==7){
+            result++;
+        }
+    }
+
+    public static int dfs(int x, int y){
+        int cnt = 1;
+        visited[x][y] = visit;
+        for(int i=0; i<4; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(boundaryCheck(nx, ny) && visited[nx][ny]!=visit && selected[nx][ny]){
+                cnt += dfs(nx,ny);
             }
         }
+        return cnt;
     }
 
     public static boolean boundaryCheck(int x, int y){
