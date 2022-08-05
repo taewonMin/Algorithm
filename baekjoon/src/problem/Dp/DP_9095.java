8da5aa27cce35af13610421a9e9c7284d2680db3
smarto <mtw9411@naver.com>
2022-08-06 00:27:26 +0900

    DP / 1,2,3 더하기

---
+++ baekjoon/src/problem/Dp/DP_9095.java
@@ -0,0 +1,32 @@
+package problem.Dp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 1,2,3 더하기
+public class DP_9095 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        int[] dp = new int[11];
+        dp[1] = 1; 
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i=4; i<11; i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+
+        for(int i=0; i<T; i++){
+            bw.write(dp[Integer.parseInt(br.readLine())]+"\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
