066746522ebd34a35f8620aecd52b23d491de7ab
smarto <mtw9411@naver.com>
2022-08-06 12:39:45 +0900

    DP / 2xn 타일링

--- baekjoon/src/problem/Dp/DP_11726.java
+++ baekjoon/src/problem/Dp/DP_11726.java
@@ -10,7 +10,7 @@ public class DP_11726 {
         int n = sc.nextInt();
         int[] dp = new int[n+1];
         dp[1] = 1;
-        dp[2] = 2;
+        if(n>1) dp[2] = 2;
 
         for(int i=3; i<=n; i++){
             dp[i] = (dp[i-1] + dp[i-2])%10007;
