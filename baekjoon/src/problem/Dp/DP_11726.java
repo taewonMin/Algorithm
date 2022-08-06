63f8efe66d99aeb2e8f540c43e69af9d3802c903
smarto <mtw9411@naver.com>
2022-08-06 12:24:02 +0900

    DP / 2xn 타일링

---
+++ baekjoon/src/problem/Dp/DP_11726.java
@@ -0,0 +1,23 @@
+package problem.Dp;
+
+import java.util.Scanner;
+
+// 2xn 타일링
+public class DP_11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%10007;
+        }
+
+        System.out.println(dp[n]);
+
+        sc.close();
+    }
+}
