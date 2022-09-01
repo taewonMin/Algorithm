34d26f363e92f85a3b81105f004d9869b62e62dc
smarto <mtw9411@naver.com>
2022-09-01 22:57:04 +0900

    DP / 제곱수의 합 - BottomUp

--- baekjoon/src/problem/Dp/DP_1699.java
+++ baekjoon/src/problem/Dp/DP_1699.java
@@ -1,30 +1,24 @@
 package problem.Dp;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 // 제곱수의 합
 public class DP_1699 {
-    public static int[] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        dp = new int[N+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        int[] dp = new int[N+1];
 
-        System.out.println(calc(N));
-
-        sc.close();
-    }
-
-    public static int calc(int n){
-        if(dp[n]==Integer.MAX_VALUE){
-            for(int i=(int)Math.sqrt(n); i>=1; i--){
-                dp[n] = Math.min(dp[n], calc(n-i*i)+1);
+        for(int i=1; i<=N; i++){
+            dp[i] = i;
+            for(int j=2; j*j<=i; j++){
+                dp[i] = Math.min(dp[i], dp[i-j*j]+1);
             }
         }
-        return dp[n];
+
+        System.out.println(dp[N]);
+
+        sc.close();
     }
 }
