f3636e73d51ba1b25bf8676c518a06e77fd68613
smarto <mtw9411@naver.com>
2022-09-04 23:09:29 +0900

    구현 / 단어 길이 재기

---
+++ baekjoon/src/problem/Implement/Imple_2743.java
@@ -0,0 +1,16 @@
+package problem.Implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 단어 길이 재기
+public class Imple_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().length());
+
+        br.close();
+    }
+}
