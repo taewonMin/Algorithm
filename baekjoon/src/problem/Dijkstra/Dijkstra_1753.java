697be4599b617c8caf8ed87c26a3c08d67916eb2
smarto <mtw9411@naver.com>
2022-09-24 00:01:19 +0900

    다익스트라 / 최단 경로

--- baekjoon/src/problem/Dijkstra/Dijkstra_1753.java
+++ baekjoon/src/problem/Dijkstra/Dijkstra_1753.java
@@ -1,8 +1,10 @@
 package problem.Dijkstra;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,13 +20,14 @@ public class Dijkstra_1753{
         int distance;
         int idx;
 
-        Node(int distance, int idx){
+        Node(int idx, int distance){
             this.distance = distance;
             this.idx = idx;
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] info = br.readLine().split(" ");
         v = Integer.parseInt(info[0]);
@@ -32,6 +35,9 @@ public class Dijkstra_1753{
         int k = Integer.parseInt(br.readLine());
 
         graph = new ArrayList[v+1];
+        for(int i=1; i<=v; i++){
+            graph[i] = new ArrayList<>();
+        }
         dis = new int[v+1];
         for(int i=1; i<=v; i++){
             if(i!=k){
@@ -43,17 +49,25 @@ public class Dijkstra_1753{
             graph[input[0]].add(new Node(input[1],input[2]));
         }
 
-        dijkstra(v);
+        dijkstra(k);
+
+        for(int i=1; i<=v; i++){
+            bw.write((dis[i]==Integer.MAX_VALUE ? "INF":dis[i])+"\n");
+        }
 
         br.close();
+        bw.flush();
+        bw.close();
     }
 
     public static void dijkstra(int start){
+        dis[start] = 0;
         PriorityQueue<Node> queue = new PriorityQueue<>((o1,o2) -> o1.distance - o2.distance);
-        queue.offer(new Node(0, start));
+        queue.offer(new Node(start, 0));
 
         while(!queue.isEmpty()){
             Node node = queue.poll();
+            if(node.distance != dis[node.idx]) continue;
 
             for(Node next : graph[node.idx]){
                 if(dis[next.idx] > dis[node.idx]+next.distance){
