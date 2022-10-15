7313a178da8d101a7c144fe0c9c648ad864f1660
smarto <mtw9411@naver.com>
2022-10-15 18:10:00 +0900

    DP / 스티커 - 경계값 체크

--- baekjoon/src/problem/Dp/DP_9465.java
+++ baekjoon/src/problem/Dp/DP_9465.java
@@ -20,8 +20,10 @@ public class DP_9465 {
             dp[0] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             dp[1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             
-            dp[0][1] += dp[1][0];
-            dp[1][1] += dp[0][0];
+            if(n>1){
+                dp[0][1] += dp[1][0];
+                dp[1][1] += dp[0][0];
+            }
             for(int i=2; i<n; i++){
                 dp[0][i] += Math.max(dp[1][i-1], dp[1][i-2]);
                 dp[1][i] += Math.max(dp[0][i-1], dp[0][i-2]);
