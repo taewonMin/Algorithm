5da1f234be0e7cbcc57ac62a8a944b9f1bebff91
smarto <mtw9411@naver.com>
2022-10-15 18:07:46 +0900

    DP / 스티커 - 2차원 배열로 개선

--- baekjoon/src/problem/Dp/DP_9465.java
+++ baekjoon/src/problem/Dp/DP_9465.java
@@ -1,34 +1,37 @@
 package problem.Dp;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 // 스티커
 public class DP_9465 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
         for(int t=0; t<T; t++){
             int n = Integer.parseInt(br.readLine());
-            int[][] sticker = new int[n][2];
-            int[][] dp = new int[n][3];
-            sticker[0] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            sticker[1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int[][] dp = new int[2][n];
+            dp[0] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            dp[1] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             
-            dp[0][1] = sticker[0][0];
-            dp[0][2] = sticker[1][0];
-            for(int i=1; i<n; i++){
-                dp[i][0] = Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][2]));
-                dp[i][1] = Math.max(dp[i-1][0], dp[i-1][2])+sticker[0][i];
-                dp[i][2] = Math.max(dp[i-1][0], dp[i-1][1])+sticker[1][i];
+            dp[0][1] += dp[1][0];
+            dp[1][1] += dp[0][0];
+            for(int i=2; i<n; i++){
+                dp[0][i] += Math.max(dp[1][i-1], dp[1][i-2]);
+                dp[1][i] += Math.max(dp[0][i-1], dp[0][i-2]);
             }
 
-            System.out.println(Math.max(dp[n-1][0], Math.max(dp[n-1][1], dp[n-1][2])));
+            bw.write(Math.max(dp[0][n-1], dp[1][n-1])+"\n");
         }
 
         br.close();
+        bw.flush();
+        bw.close();
     }
 }
