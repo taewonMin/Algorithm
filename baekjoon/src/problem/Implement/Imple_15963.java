48e125292ab5d84a02cca2a899af8063807a84a0
taewonMin <mtw9411@naver.com>
2022-10-19 17:36:38 +0900

    구현 / CASIO

---
+++ baekjoon/src/problem/Implement/Imple_15963.java
@@ -0,0 +1,16 @@
+package problem.Implement;
+
+import java.util.Scanner;
+
+// CASIO
+public class Imple_15963{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+
+        System.out.println(N==M ? 1:0);
+        sc.close();
+    }
+}
